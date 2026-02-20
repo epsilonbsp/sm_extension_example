5c4376eb849316a3e35fc65f85766d7fa6bd9f7b
epsilonbsp <deltasampler@gmail.com>
2026-02-20 17:03:03 +0200

    add build script for windows

---
+++ build.bat
@@ -0,0 +1,51 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+cls
+
+if "%~1" == "install" (
+    if not exist build mkdir build
+    pushd build
+
+    if not exist sdk_root (
+        mkdir sdk_root
+        pushd sdk_root
+
+        git clone https://github.com/alliedmodders/sourcemod --recursive
+        git clone https://github.com/alliedmodders/metamod-source
+        git clone https://github.com/alliedmodders/hl2sdk hl2sdk-css -b css
+        git clone https://github.com/alliedmodders/ambuild
+
+        python -m venv venv
+        call venv\Scripts\activate
+        python -m pip install ./ambuild
+        call deactivate
+
+        popd
+    )
+
+    popd
+) else if "%~1" == "build" (
+    set ROOT_PATH=%~dp0
+    set ROOT_PATH=!ROOT_PATH:~0,-1!
+    if not exist build\output mkdir build\output
+
+    call build\sdk_root\venv\Scripts\activate
+
+    pushd build\output
+
+    python ..\..\configure.py ^
+        --sm-path=!ROOT_PATH!\build\sdk_root\sourcemod ^
+        --mms-path=!ROOT_PATH!\build\sdk_root\metamod-source ^
+        --hl2sdk-root=!ROOT_PATH!\build\sdk_root ^
+        --hl2sdk-manifest-path=!ROOT_PATH!\build\sdk_root\sourcemod\hl2sdk-manifests ^
+        --sdks=css
+
+    ambuild
+
+    deactivate
+
+    popd
+) else (
+    echo No valid command was specified
+)
--- readme.md
+++ readme.md
@@ -1,13 +1,16 @@
 # SourceMod Extension Example
 Showcase of how to build [SourceMod Sample Extension](https://github.com/alliedmodders/sourcemod/tree/master/public/sample_ext) using [AMBuild](https://github.com/alliedmodders/ambuild)
 # Requirements
+### Windows
+* Download and install [Git](https://git-scm.com/install/windows)
+* Download and install [Python](https://www.python.org/downloads/)
 ### Linux
 * Install Dependencies
 
       sudo apt install git python3 python-is-python3 python3-pip python3.12-venv
 # Usage
     # Install SDK
-    ./build.sh install
+    .\build.bat install
 
     # Build extension
-    ./build.sh build
+    .\build.bat build
